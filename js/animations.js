f0b1f7ca2e7282d82ac1e303893430c51036bf26
John Doe <a.kykylin@gmail.com>
2023-03-19 13:48:23 +0400

    19.03

--- js/animations.js
+++ js/animations.js
@@ -1,29 +1,3 @@
-const INCREASE_NUMBER_ANIMATION_SPEED = 50;
-
-function increaseNumberAnimationStep (i, element, endNumber) {
-  if (i <= endNumber) {
-    if (i === endNumber) {
-      element.innerText = i + '+';
-    } else {
-      element.innerText = i;
-    }
-
-      i+=100;
-
-      setTimeout(function() {
-        increaseNumberAnimationStep(i, element, endNumber);
-      }, INCREASE_NUMBER_ANIMATION_SPEED);
-    }
-  }
-
-function initIncreaseNumberAnimation() {
-  const element = document.querySelector('.features__clients-count');
-  
-  increaseNumberAnimationStep(0, element, 5000);
-}
-
-initIncreaseNumberAnimation();
-
 document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
   if (event.target.value === 'other') {
     const formContainer = document.createElement('div');
@@ -35,7 +9,7 @@ document.querySelector('#budget').addEventListener('change', function handleSele
     input.type = "text"; 
       
     formContainer.appendChild(input);
-    document.querySelector('#form form').insertBefore(formContainer, document.querySelector('.form__submit'));
+    document.querySelector('#form form').insertBefore(formContainer, document.querySelector('.form__submit')); 
   }
  
   const otherInput = document.querySelector('.form__other-input');
@@ -43,3 +17,32 @@ document.querySelector('#budget').addEventListener('change', function handleSele
   document.querySelector('#form form').removeChild(otherInput); 
   }
 });
+
+
+
+const INCREASE_NUMBER_ANIMATION_SPEED = 50;
+
+function increaseNumberAnimationStep(i, element, endNumber) {
+  if (i <= endNumber) {
+    if (i === endNumber) {
+      element.innerText = i + '+';
+    } else {
+      element.innerText = i;
+    }
+
+    i += 100;
+
+    setTimeout(function () {
+      increaseNumberAnimationStep(i, element, endNumber);
+    }, INCREASE_NUMBER_ANIMATION_SPEED);
+  }
+}
+
+function initIncreaseNumberAnimation() {
+  const element = document.querySelector('.features__clients-count');
+
+  increaseNumberAnimationStep(0, element, 5000);
+}
+
+initIncreaseNumberAnimation();
+
